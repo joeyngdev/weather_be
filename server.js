7a5026e432ffc9651d77832011867de64cca526b
joeyngdev <trtinnguyen2009@gmail.com>
2024-12-14 01:29:12 +0700

    commit

--- server.js
+++ server.js
@@ -94,6 +94,6 @@ cron.schedule('*/1 * * * *', async () => {
     await sendEmail(); 
     users = [];
   });
-  app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:${3000}`);
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
   });
